55edd9f978d2eff48b19d6049b6aef63ba35f047
jayseth <46541147+jayseth@users.noreply.github.com>
2021-10-06 15:39:59 +0530

    Adding ECS to variables

--- variables.tf
+++ variables.tf
@@ -76,3 +76,32 @@ variable "instance_Type"{
   description = "Type of instance"
   type = string
 }
+
+
+
+#####################################################################################
+###################################  ECS  ###########################################
+
+variable "cluster_name" {
+  description = "Cluster name."
+}
+
+variable "name_prefix" {
+  description = "A prefix used for naming resources."
+  type        = string
+}
+
+variable "subnets_ids" {
+  description = "IDs of subnets. Use subnets from various availability zones to make the cluster more reliable."
+  type        = list(string)
+}
+
+variable "task_container_image" {
+  description = "The image used to start a container."
+  type        = string
+}
+
+variable "trusted_cidr_blocks" {
+  description = "Trusted subnets e.g. with ALB and bastion host."
+  type        = list(string)
+}
