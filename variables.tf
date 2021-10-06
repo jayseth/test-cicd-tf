b2068932e5ae9df29e964afcf021876b4d59a852
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-06 18:14:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,55 @@ variable "trusted_cidr_blocks" {
   description = "Trusted subnets e.g. with ALB and bastion host."
   type        = list(string)
 }
+
+
+####################################################################################
+####################################### vpc-mod ####################################
+
+variable "cidr_vpc" {
+    type=string
+  
+}
+
+variable "public_subnet" {
+  type=string
+}
+
+variable "private_subnet" {
+    type=string
+  
+}
+
+variable "vpc_subnet_az" {
+    type=string
+  
+}
+
+
+variable "network_acl_egress" {
+    description = "values for egress network acl "
+    type = map(any)
+    
+  
+}
+
+variable "network_acl_ingress" {
+    description = "values for inegress network acl "
+    type = map(any)
+   
+  
+}
+
+variable "security_group_ingress" {
+    description = "values for inegress security group "
+    type = map(any)
+   
+  
+}
+
+variable "security_group_egress" {
+    description = "values for egress security group "
+    type = map(any)
+   
+  
+}
