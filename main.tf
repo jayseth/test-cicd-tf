53673f2777ca6f5bfa5c369f2f3c8cdfbf84b0bc
jayseth <46541147+jayseth@users.noreply.github.com>
2021-10-06 16:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "ECS" {
 
     cluster_name = var.cluster_name
     name_prefix = var.name_prefix
-    subnets_ids = var.subnet_ids
+    subnets_ids = var.subnets_ids
     task_container_image = var.task_container_image
     trusted_cidr_blocks = var.trusted_cidr_blocks
 }
