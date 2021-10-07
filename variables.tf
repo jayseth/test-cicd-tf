1d9c5465a4c385b20110dc4027af23611459b241
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-07 17:54:52 +0530

    Updates to variables.tf with fsxWindows module

--- variables.tf
+++ variables.tf
@@ -363,3 +363,65 @@ locals {
   az_count = length(var.availability_zones)
 }
 
+#####################################################################################
+####################################### fsx Windows #################################
+
+
+
+
+variable "mod_fsx_win_capacity" {
+    description = "fsx windows file system capacity"
+    default = 300
+  
+}
+
+variable "mod_fsx_win_throughtput_capacity" {
+
+    description = "fsx windows file system throughtput capacity"
+    default = 1024
+  
+}
+
+variable "mod_fsx_vpc_cidr" {
+    description = "vpc id for fsx windows directory"
+    type=string
+  
+}
+
+variable "mod_fsx_win_subids" {
+
+    description = "list of subnet ids for the fsx windows file system"
+    type=list(string)
+  
+}
+
+variable "fsx_win_sub_az" {
+    description = "sets the AZ for subnets"
+    type = list(string)
+  
+}
+
+variable "mod_serv_directory_name" {
+    description ="service directory name"
+    type = string
+  
+}
+
+variable "mod_serv_directory_password" {
+    description = "service directory password"
+     type = string
+  
+}
+
+variable "mod_serv_directory_size" {
+  
+  description = "service directory size"
+   type = string
+}
+
+variable "mod_serv_directory_type" {
+    description = "service directory type"
+    type = string
+  
+}
+
