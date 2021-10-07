80705ed6549a102184e853f0784c0e3e460ff1a5
riz2rishi <42760938+riz2rishi@users.noreply.github.com>
2021-10-07 17:42:12 +0530

    EC2 Module added

--- variables.tf
+++ variables.tf
@@ -202,3 +202,164 @@ variable "mod_lb_listener_rule_priority" {
 
 }
 
+#####################################################################################
+####################################### ec2_mod #####################################
+
+#variables for instance creation
+variable "region"{
+    description = " Name of region "
+    type = string
+       
+}
+variable "profile"{
+  description = "Name of profile"
+  type = string
+  
+}
+variable "enable_instance" {
+  type = bool
+  description = " Want to deploy EC2 instance ? (true / false)"
+}
+variable "ami"{
+  description = "AMI ID "
+  type = map(any)
+    
+}
+variable "instanceType"{
+  description = "Type of instance"
+  type = string
+  default= "t2.micro"
+}
+
+variable "index" {
+    description = "Index number"
+    type = number
+    default=0
+}
+
+#-------------------------------------------------------------------------------------------------------
+
+
+
+#Golden Image   (need to put list of AZs)
+
+variable "az" {
+    description = "Name of AZ"
+  type = map(any)
+  
+}
+variable "enable_custom_ami" {
+  type = bool
+  description = " Want to deploy custom AMI ? (true / false)"
+}
+variable "size" {
+    description = "Size"
+  type = number
+  default=  40
+}
+variable "rootDevName" {
+    description = "Root Device Name"
+  type = string
+  default=  "/dev/xvda"
+}
+variable "DevName" {
+  description = "Device name of block"
+  type = string
+  default = "/dev/xvda"
+}
+
+#------------------------------------------------------------------------------------------------------------
+
+
+#ASG Variables
+variable "enable_asg" {
+  description = "Want to deploy Auto Scaling Groups ? (true/false)"
+  type = bool
+}
+variable "des_capacity" {
+  description = "Desired capacity of ASG"
+  type = number
+  default = 1
+}
+variable "maxSize" {
+  description = "Maximum size of ASG"
+  type = number
+  
+}
+variable "minSize" {
+  description = "Minimum size of ASG"
+  type = number
+  
+}
+
+variable "namePrefix" {
+  description = "Name Prefix of AWS launch Template"
+  type = string
+  
+}
+
+variable "imgID" {
+  description = "Image ID of launch template"
+  type = string
+  
+}
+
+
+#----------------------------------------------------------------------------------------
+
+
+
+#Auto Start - Stop
+
+
+variable "timeout" {
+  type = string
+  description = " Duration for timeout"
+  
+}
+variable "enable_auto_start_stop" {
+  type = bool 
+  description = "Want to enable auto start - stop functionality for EC2 instance ? (true /false)"
+}
+
+
+#------------------------------------Auto Sessions Manager--------------------------------------
+
+variable "enable_auto_sess_manager" {
+  description = "Want to enable Automatic Sessions Manager ? (true/false)"
+  type = bool
+}
+variable "availability_zones" {
+  description = "AWS AVailability Zones"
+  type        = list
+}
+
+variable "name" {
+  description = "Application Name"
+  type        = string
+}
+
+variable "public_start_ip" {
+  description = "Start ip of public subnets"
+  type        = string
+}
+
+variable "private_start_ip" {
+  description = "Start ip of private subnets"
+  type        = string
+}
+
+variable "subnet_prefix" {
+  description = "Prefix for setup of subnets"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR of the VPC"
+  type        = string
+}
+
+locals {
+  az_count = length(var.availability_zones)
+}
+
