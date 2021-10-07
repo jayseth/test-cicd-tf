2195287edba69db0ea81df03f353416526635854
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-07 17:11:09 +0530

    Updates to variaables.tf with org variables

--- variables.tf
+++ variables.tf
@@ -26,6 +26,53 @@ variable "trusted_cidr_blocks" {
 }
 
 
+#####################################################################################
+####################################### ORG #####################################
+
+
+
+variable "org_enabled_policy" {
+     type = list(string)
+  
+}
+
+variable "enable_acc_sa" {
+    description = "To enable the creation of account for Security Account OU"
+    
+    type = bool
+  
+}
+
+variable "enable_acc_log" {
+    description = "To enable the creation of account for Log Archive OU"
+    
+     type = bool
+  
+}
+
+variable "enable_acc_ssa" {
+    description = "To enable the creation of account for Shared Services Account OU"
+    
+     type = bool
+  
+}
+
+
+variable "aws_account_Org_usr"{
+    description = "name for the organization account"
+    
+    type=list(string)  
+}
+
+variable "aws_account_Org_usr_email"{
+    description = "email for the organization account"
+    
+    type=list(string)
+}
+
+
+
+
 ####################################################################################
 ####################################### vpc-mod ####################################
 
