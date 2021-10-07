8c35562b44cfd67ea694ca2355414c5b1c039954
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-07 16:27:06 +0530

    Updates to variables.tf with alb variables

--- variables.tf
+++ variables.tf
@@ -107,3 +107,46 @@ variable "mod_aws_vpc_sub_az"{
     type = string
 }
 
+#####################################################################################
+####################################### ALB #####################################
+
+variable "mod_lb_enable_deletion_protection" {
+    description = "deletion protection for ALB"
+    type = bool
+  
+}
+
+variable "mod_lb_type" {
+    description = "Load Balancer Type"
+    type = string
+
+  
+}
+
+variable "mod_aws_vpc_cidr" {
+    description = "vpc cidr for ALB"
+    type = string
+  
+}
+
+variable "mod_lb_subnet_count" {
+    description = "Number of subnets to be associated with the Lb"
+    
+  
+}
+variable "mod_public_subnet_cidr" {
+     description="List Public subnets"
+     type=list(string)
+  
+}
+variable "mod_alb_vpc_sub_az" {
+    description="List availability zones for lb"
+    type=list(string)
+  
+}
+
+variable "mod_lb_listener_rule_priority" {
+   description="Sets the rule priority for lb listener"
+
+}
+
