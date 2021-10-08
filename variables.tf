ce30f4527ece5f9060ecc3d0def1a1305c2f0414
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-08 11:43:16 +0530

    Updates to variables.tf with DynamoDB and DAX

--- variables.tf
+++ variables.tf
@@ -720,3 +720,124 @@ variable "mod_nlb_listener_rule_priority" {
 
 }
 
+
+
+#####################################################################################
+####################################### DYNAMO DB ###################################
+
+
+variable "billing_mode" {
+
+    description = "Dynamo DB Billing Mode"
+    
+    type=string  
+}
+
+variable "db_name" {
+
+    description = "Dynamo DB Name"
+  
+    type = string
+  
+}
+
+variable "hash_key" {
+
+    description = "hash key for db"
+  
+    type = string
+
+  
+}
+
+variable "range_key" {
+
+    description = "range key for db"
+   
+    type=string
+  
+}
+
+variable "ttl_enable" {
+    description = "Indicates whether ttl is enabled (true) or disabled (false)"
+   
+    type=bool
+  
+}
+
+variable "ttl_attribute_name" {
+
+    description = " The name of the table attribute to store the TTL timestamp in"
+    
+    type = string
+  
+}
+
+variable "read_capacity" {
+
+    description = "The number of read units for this table"
+ 
+
+  
+}
+
+variable "write_capacity" {
+
+    description = "The number of write units for this table"
+   
+  
+}
+
+variable "stream_enabled_dynamodb" {
+
+    description = "Sets the streams to be enabled or disabled"
+  
+    type=bool
+  
+}
+
+variable "stream_view_type_dynamodb" {
+
+    description = "Sets the information that is written to the stream when table is modified( Options:KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES)"
+    
+    type = string
+  
+}
+
+
+
+
+#####################################################################################
+####################################### DAX #########################################
+
+variable "dax_cluster_name" {
+
+    description = "Sets the name of the dax cluster"
+    
+    type = string
+  
+}
+variable "dax_node_type" {
+
+    description = "Sets the compute and memory capacity of the nodes"
+    
+    type = string
+  
+}
+
+variable "dax_replication_factor" {
+
+    description = "Sets the number of nodes for DAX cluster"
+    
+  
+}
+
+variable "dax_az" {
+
+    description = "list of availibility zones in which the custers will be formed"
+   
+    type=list(string)
+  
+}
+
+
