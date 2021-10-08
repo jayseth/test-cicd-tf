8d2e9bc2435e236ec974f7070bc2b8085d806d51
ArulSaxena <48524441+ArulSaxena@users.noreply.github.com>
2021-10-08 18:35:08 +0530

    Update alb_aws_instance.tf

--- module/ALB/alb_aws_instance.tf
+++ module/ALB/alb_aws_instance.tf
@@ -5,7 +5,7 @@
 */
 
 resource "aws_instance" "test" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.lb_subnet[0].id
   
